5b4266e6851204e0eef2d706e228457e9471faa6
Andy Perdana <geruhn@hotmail.com>
2013-10-13 22:21:18 +0200

    Added basic funtionality for issue #100 TODO: - implementing hook to actually send mail when user registers - switch to db query for 'isAdmin' instead of conf.get('adminEMailList');

--- test/mailsender/mailsenderAPI_test.js
+++ test/mailsender/mailsenderAPI_test.js
@@ -3,7 +3,9 @@
 var expect = require('chai').expect;
 var sinon = require('sinon').sandbox.create();
 var beans = require('../configureForTest').get('beans');
-//var Message = beans.get('message');
+var conf = require('../configureForTest');
+var Member = conf.get('beans').get('member');
+var Message = conf.get('beans').get('message');
 
 var membersAPI = beans.get('membersAPI');
 var groupsAPI = beans.get('groupsAPI');
@@ -11,6 +13,24 @@ var activitiesAPI = beans.get('activitiesCoreAPI');
 //var groupsAndMembersAPI = beans.get('groupsAndMembersAPI');
 
 var api = beans.get('mailsenderAPI');
+
+function newTestSender() {
+  return new Member({
+    firstname: 'Andy',
+    nickname: 'me',
+    lastname: 'Sender',
+    email: 'geruhn@hotmail.com'
+  });
+}
+
+function newTestRecipient() {
+  return new Member({
+    firstname: 'Andy',
+    nickname: 'me',
+    lastname: 'Empfänger',
+    email: 'der.geruhn@gmail.com'
+  });
+}
 
 describe('MailsenderAPI', function () {
   var activityURL = 'acti_vi_ty';
@@ -22,7 +42,10 @@ describe('MailsenderAPI', function () {
     sinon.stub(activitiesAPI, 'getActivity', function (activityURL, callback) {
       callback(null, {markdown: function () {} });
     });
-    sinon.stub(membersAPI, 'getMember', function (nickname, callback) { callback(null, {}); });
+    sinon.stub(membersAPI, 'getMember',
+      function (nickname, callback) {
+        callback(null, newTestRecipient());
+      });
     done();
   });
 
@@ -30,6 +53,7 @@ describe('MailsenderAPI', function () {
     sinon.restore();
     done();
   });
+
 
   it('collects data for showing the edit form for an activity', function (done) {
     api.dataForShowingMessageForActivity(activityURL, function (err, result) {
@@ -51,5 +75,46 @@ describe('MailsenderAPI', function () {
     });
   });
 
+  it('sends a mail', function (done) {
+    this.timeout(50000);
+    var member = newTestSender();
+    var message = new Message({
+      subject: 'Betreff',
+      markdown: 'Juchuh!',
+      htmlAddOn: ''
+    }, member);
+    api.sendMailToMember('*andy', message, function () {
+      console.log('Callback of sending mail called');
+      done();
+    });
+  });
+
+  it('sends a mail to an adress list', function (done) {
+    this.timeout(500000);
+    var robot = new Member({
+      firstname: 'I,',
+      nickname: '',
+      lastname: 'Robot',
+      email: conf.get('sender-address')
+    });
+    var message = new Message({
+      subject: 'Rundmail an Adressliste',
+      markdown: 'asdf Blabla, \n Whatever, dude.',
+      htmlAddOn: ''
+    }, robot);
+    var adressList = ['der.geruhn@gmail.com', 'geruhn@hotmail.com'];
+    api.sendMailToAdressList(adressList, message, function () {
+      console.log('Callback of sending adress list called');
+      done();
+    });
+  });
+
+  it('sends a mail to the adminList because we got a new member', function (done) {
+    this.timeout(500000);
+    api.sendMailForNewMember(newTestSender(), function () {
+      console.log('Mail to Adminlist is done.');
+      done();
+    });
+  });
 });
 
